3c56a3e66da04820aa9f791f5f01d4b33d864a8d
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-05-17 04:24:00 -0300

    Adds git mappings to which-key

--- lua/user/which-key-mappings.lua
+++ lua/user/which-key-mappings.lua
@@ -5,12 +5,15 @@ if not ok then
 end
 
 wk.register ({
+  -- FIND
   f = {
     name='Find',
     f = { '<cmd>Telescope find_files<cr>', 'Find File' }, -- create a binding with label
     b = { '<cmd>Telescope buffers<cr>', 'Find Buffers' },
     g = { '<cmd>Telescope find_files<cr>', 'Find GIT files' }, -- create a binding with label
   },
+
+  -- LSP
   l = {
     name='LSP',
     l = { '<cmd>Telescope diagnostics<cr>', 'Show Diagnostics' },
@@ -21,8 +24,13 @@ wk.register ({
     d = { '<cmd>Telescope lsp_definitions<cr>', 'Find Definitions' }, -- create a binding with label
     r = { '<cmd>Telescope lsp_references<cr>', 'Find References' }, -- create a binding with label
   },
+
+  -- GIT
   g = {
     name = 'GIT',
-    g = { '<cmd>Neogit<cr>', 'Open Neogit' }
+    g = { '<cmd>Neogit<cr>', 'Open Neogit' },
+    s = { '<cmd>DiffviewOpen<cr>', 'File History' },
+    h = { '<cmd>DiffviewFileHistory<cr>', 'File History' },
+    x = { '<cmd>DiffviewClose<cr>', 'Close Diffview' },
   }
 }, { prefix = '<leader>' })
