93199e3cf895e727908f1bb9c316b3a192ec267b
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-07-17 17:34:57 -0300

    Reenable transparency

--- lua/user/transparent.lua
+++ lua/user/transparent.lua
@@ -4,7 +4,7 @@ if not transparent_satus_ok then
 end
 
 transparent.setup({
-  enable = false, -- boolean: enable transparent
+  enable = true, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to "all", that means all available groups
 
