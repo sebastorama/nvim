a15db72b7cd3639574e63c1a9ac2e15f5fd09559
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-07-24 22:48:23 -0300

    Couple of colorscheme changes

--- lua/user/color.lua
+++ lua/user/color.lua
@@ -1,25 +1,11 @@
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- require("transparent").setup({
---   enable = true, -- boolean: enable transparent
---   extra_groups = { -- table/string: additional groups that should be cleared
---     -- In particular, when you set it to 'all', that means all available groups
---
---     -- example of akinsho/nvim-bufferline.lua
---     "BufferLineTabClose",
---     "BufferlineBufferSelected",
---     "BufferLineFill",
---     "BufferLineBackground",
---     "BufferLineSeparator",
---     "BufferLineIndicatorSelected",
---   },
---   exclude = {}, -- table: groups you don't want to clear
--- })
+vim.g.tokyonight_day_brightness = 0.1
+vim.g.tokyonight_transparent = true
 
 vim.cmd([[set bg=dark]])
 -- require("colorbuddy").colorscheme("gruvbuddy")
 vim.cmd([[colorscheme tokyonight]])
 vim.cmd([[hi Comment guifg=#6874A9]])
-vim.cmd([[hi LineNr guifg=#202435]])
+vim.cmd([[hi LineNr guifg=#4d4f5d]])
