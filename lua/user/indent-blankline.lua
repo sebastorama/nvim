1f76e8d077305b0daf480ba8b5aa6d1bbb7d0531
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-08-12 00:36:09 -0400

    Underline on context start is distracting

--- lua/user/indent-blankline.lua
+++ lua/user/indent-blankline.lua
@@ -11,5 +11,5 @@ vim.cmd([[highlight IndentBlanklineIndent2 guibg=#303030 gui=nocombine]])
 indent_blankline.setup({
   -- for example, context is off by default, use this to turn it on
   show_current_context = true,
-  show_current_context_start = true,
+  show_current_context_start = false,
 })
