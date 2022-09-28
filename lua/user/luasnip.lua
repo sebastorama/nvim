263c74c14840da2aafe98231742da29b003303af
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-09-27 23:48:57 -0300

    My first lua snippet

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -1,7 +1,29 @@
 local ok, luasnip = pcall(require, "luasnip")
+
 if not ok then
   return
 end
+
+---@diagnostic disable: unused-local
+local s = luasnip.snippet
+local sn = luasnip.snippet_node
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local f = luasnip.function_node
+local c = luasnip.choice_node
+local d = luasnip.dynamic_node
+local r = luasnip.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.expand_conditions")
+---@diagnostic enable: unused-local
 
 -- load my custom snippets folder
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
@@ -19,3 +41,65 @@ vim.keymap.set({ "i", "s" }, "<c-k>", function()
     luasnip.jump(-1)
   end
 end, { silent = true })
+
+vim.keymap.set({ "i" }, "<c-l>", function()
+  if luasnip.choice_active() then
+    luasnip.change_choice(1)
+  end
+end, { silent = true })
+
+luasnip.setup({
+  history = true,
+  -- Update more often, :h events for more info.
+  update_events = "TextChanged,TextChangedI",
+  -- Snippets aren't automatically removed if their text is deleted.
+  -- `delete_check_events` determines on which events (:h events) a check for
+  -- deleted snippets is performed.
+  -- This can be especially useful when `history` is enabled.
+  delete_check_events = "TextChanged",
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "➡️ choice node", "Comment" } },
+      },
+    },
+  },
+  -- treesitter-hl has 100, use something higher (default is 200).
+  ext_base_prio = 300,
+  -- minimal increase in priority.
+  ext_prio_increase = 1,
+  enable_autosnippets = true,
+  -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+  -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+  store_selection_keys = "<Tab>",
+  -- luasnip uses this function to get the currently active filetype. This
+  -- is the (rather uninteresting) default, but it's possible to use
+  -- eg. treesitter for getting the current filetype by setting ft_func to
+  -- require("luasnip.extras.filetype_functions").from_cursor (requires
+  -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+  -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+  ft_func = function()
+    return vim.split(vim.bo.filetype, ".", true)
+  end,
+})
+
+luasnip.add_snippets(nil, {
+  lua = {
+    -- pcall import
+    s(
+      "pcall",
+      fmt(
+        [[
+local {}_ok, {} = pcall(require, "{}")
+
+if not {}_ok then
+  return
+end
+
+      ]],
+        { i(1), rep(1), rep(1), rep(1) }
+      )
+    ),
+    --
+  },
+}, { key = "global_snippets" })
