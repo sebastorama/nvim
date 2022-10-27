2566d20a6a98aca7a27a56c2c9797fa4059d65f0
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2022-10-27 16:27:31 -0400

    Adds keymaps for reordering tabs

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,12 +22,14 @@ keymap("n", "<leader>S", ":setlocal spell!<CR>", opts)
 -- Navigate tabs and buffers
 keymap("n", "<S-l>", ":tabn<CR>", opts)
 keymap("n", "<S-h>", ":tabp<CR>", opts)
+
+keymap("n", "<Left>", ":tabm -1<CR>", opts)
+keymap("n", "<Right>", ":tabm +1<CR>", opts)
+
 keymap("n", "<S-Left>", ":bn<CR>", opts)
 keymap("n", "<S-Right>", ":bp<CR>", opts)
 
 -- Arrow keys for resizing
-keymap("n", "<Left>", ":vertical resize -3<CR>", opts)
-keymap("n", "<Right>", ":vertical resize +3<CR>", opts)
 keymap("n", "<Up>", ":resize +3<CR>", opts)
 keymap("n", "<Down>", ":resize -3<CR>", opts)
 
