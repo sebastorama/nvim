c89f9dc8de8ed0868dfde3bc4a8834e6ff3a71a7
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-02-04 18:22:36 -0400

    Ntree tweaks

--- lua/user/ntree.lua
+++ lua/user/ntree.lua
@@ -31,7 +31,7 @@ require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
   },
   renderer = {
     indent_markers = {
-      enable = false,
+      enable = true,
       icons = {
         corner = "└ ",
         edge = "│ ",
@@ -40,6 +40,39 @@ require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
     },
     icons = {
       webdev_colors = true,
+      git_placement = "after",
+    },
+    show = {
+      file = true,
+      folder = true,
+      folder_arrow = true,
+      git = true,
+      modified = true,
+    },
+    glyphs = {
+      default = "",
+      symlink = "",
+      bookmark = "",
+      modified = "●",
+      folder = {
+        arrow_closed = "",
+        arrow_open = "",
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+        symlink_open = "",
+      },
+      git = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "",
+        ignored = "◌",
+      },
     },
   },
   hijack_directories = {
