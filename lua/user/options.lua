04f8c40dbfc4287356a6beb0591e5b1801d22e7b
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-04-04 12:08:29 -0400

    Filepath on winbar relative to working directory

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -42,7 +42,7 @@ vim.opt.updatetime = 300 -- faster completion (4000ms default)
 vim.opt.wrap = false -- display lines as one long line
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 
-vim.o.winbar = "%F%m%=%n"
+vim.o.winbar = "%f%m%=%n"
 vim.o.laststatus = 3
 
 vim.cmd([[
