0267eb9aa65f983dbdb23572e743d04f5a56779e
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-10-28 17:40:55 -0400

    Moved from packer.nvim to lazy.nvim

--- init.lua
+++ init.lua
@@ -1,6 +1,22 @@
 require("user.globals")
 
-local modules = {
+-- Lazy nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("user.plugins")
+
+ local modules = {
   "user.options",
   "user.aerial",
   "user.autocmd",
@@ -21,7 +37,6 @@ local modules = {
   "user.ntree",
   "user.nvim-dap",
   "user.nvim-dap-ui",
-  "user.plugins",
   "user.rust-tools",
   "user.spectre",
   "user.tabby",
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,110 +1,86 @@
-vim.cmd([[packadd packer.nvim]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
-return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-  use({
-    "nvim-telescope/telescope.nvim",
-    requires = "nvim-lua/plenary.nvim",
-  })
-  use({ "nvim-telescope/telescope-ui-select.nvim" })
-  use("nanozuki/tabby.nvim")
-
-  use("tpope/vim-surround")
-
-  use("windwp/nvim-autopairs")
-  use({
+require("lazy").setup({
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  "nvim-telescope/telescope-ui-select.nvim",
+  "tpope/vim-surround",
+  "windwp/nvim-autopairs",
+  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
     end,
-  })
-  use("junegunn/vim-easy-align")
-
-  use("mhinz/vim-grepper")
-  use({
-    "junegunn/fzf",
-    run = function()
-      vim.fn["fzf#install"]()
-    end,
-  })
-  use("kazhala/close-buffers.nvim")
+  },
+  "junegunn/vim-easy-align",
+  "folke/which-key.nvim",
+  "mhinz/vim-grepper",
+  "kazhala/close-buffers.nvim",
 
   -- GIT
-  use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
-  use({
-    "sindrets/diffview.nvim",
-    requires = "nvim-lua/plenary.nvim",
-  })
+  { "TimUntersberger/neogit", dependencies = "nvim-lua/plenary.nvim" },
+  { "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
 
   -- UI
-  use("nvim-pack/nvim-spectre")
-  use("folke/which-key.nvim")
-  use("kyazdani42/nvim-tree.lua")
-  use("kyazdani42/nvim-web-devicons")
-  use("norcalli/nvim-colorizer.lua")
-  use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
-  use("nvim-lualine/lualine.nvim")
-  use("ziontee113/neo-minimap")
-  use("folke/todo-comments.nvim")
-  use("lewis6991/gitsigns.nvim")
+  "nvim-pack/nvim-spectre",
+  "folke/which-key.nvim",
+  "kyazdani42/nvim-tree.lua",
+  "kyazdani42/nvim-web-devicons",
+  "norcalli/nvim-colorizer.lua",
+  { "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
+  "nvim-lualine/lualine.nvim",
+  "ziontee113/neo-minimap",
+  "folke/todo-comments.nvim",
+  "lewis6991/gitsigns.nvim",
+  "nanozuki/tabby.nvim",
 
   -- dadbod
-  -- use("tpope/vim-dadbod")
-  -- use("kristijanhusak/vim-dadbod-ui")
-  -- use("kristijanhusak/vim-dadbod-completion")
-  -- use("vim-scripts/dbext.vim")
+  "tpope/vim-dadbod",
+  "kristijanhusak/vim-dadbod-ui",
+  "kristijanhusak/vim-dadbod-completion",
+  "vim-scripts/dbext.vim",
 
   -- Colorschemes
-  use("NLKNguyen/papercolor-theme")
-  use("folke/tokyonight.nvim")
-  use("ellisonleao/gruvbox.nvim")
-  use("tjdevries/colorbuddy.nvim")
-  use("tjdevries/gruvbuddy.nvim")
-  use("navarasu/onedark.nvim")
-  use("Mofiqul/vscode.nvim")
-  use("ayu-theme/ayu-vim")
-  use("Th3Whit3Wolf/one-nvim")
-  use({ "catppuccin/nvim", as = "catppuccin" })
-  use("shaunsingh/solarized.nvim")
+  "NLKNguyen/papercolor-theme",
+  "folke/tokyonight.nvim",
+  "ellisonleao/gruvbox.nvim",
+  "tjdevries/colorbuddy.nvim",
+  "tjdevries/gruvbuddy.nvim",
+  "navarasu/onedark.nvim",
+  "Mofiqul/vscode.nvim",
+  "ayu-theme/ayu-vim",
+  "Th3Whit3Wolf/one-nvim",
+  { "catppuccin/nvim", as = "catppuccin" },
+  "shaunsingh/solarized.nvim",
 
   -- Completion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-cmdline")
-  use("saadparwaiz1/cmp_luasnip")
-  use("hrsh7th/cmp-calc")
-  use("f3fora/cmp-spell")
-  use("ray-x/lsp_signature.nvim")
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-calc",
+  "f3fora/cmp-spell",
+  "ray-x/lsp_signature.nvim",
 
   -- LSP & TreeSitter
-  use("neovim/nvim-lspconfig") -- enable LSP
-  use("williamboman/nvim-lsp-installer") -- simple to use language server installer
-  use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-  use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-  use("nvim-lua/plenary.nvim")
-  use("jose-elias-alvarez/typescript.nvim")
-  use("stevearc/aerial.nvim")
-  use("windwp/nvim-ts-autotag")
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use({ "nvim-treesitter/playground" })
-  use("nvim-treesitter/nvim-treesitter-context")
-  use("github/copilot.vim")
-  use("simrat39/rust-tools.nvim")
+  "neovim/nvim-lspconfig", -- enable LSP
+  "williamboman/nvim-lsp-installer", -- simple to use language server installer
+  "tamago324/nlsp-settings.nvim", -- language server settings defined in json for
+  "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
+  "nvim-lua/plenary.nvim",
+  "jose-elias-alvarez/typescript.nvim",
+  "stevearc/aerial.nvim",
+  "windwp/nvim-ts-autotag",
+  { "nvim-treesitter/nvim-treesitter" },
+  { "nvim-treesitter/playground" },
+  "nvim-treesitter/nvim-treesitter-context",
+  "github/copilot.vim",
+  "simrat39/rust-tools.nvim",
 
   -- DAP
-  use("mfussenegger/nvim-dap")
-  -- use("nvim-telescope/telescope-dap.nvim")
-  -- use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+  "mfussenegger/nvim-dap",
+  -- "nvim-telescope/telescope-dap.nvim",
+  -- { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
 
   -- snippets
-  use("L3MON4D3/LuaSnip")
-  -- use("rafamadriz/friendly-snippets")
-end)
+  "L3MON4D3/LuaSnip",
+})
