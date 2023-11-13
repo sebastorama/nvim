df55149149eea07b283fa745f542474e41030405
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-11-13 11:53:40 -0300

    To much setup for a single keymap set

--- ftplugin/zsh.lua
+++ ftplugin/zsh.lua
@@ -1,6 +1,1 @@
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-
-vim.cmd([[startinsert!]])
-
-keymap("i", "<S-CR>", "<ESC>ZZ", opts)
+vim.api.nvim_set_keymap("i", "<S-CR>", "<ESC>ZZ", { noremap = true, silent = true })
