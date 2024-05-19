ca6c8574d5b3f1d557740b4c26397ffa6843f4d0
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2024-05-19 18:45:54 -0300

    Disable dap-virtual-text

--- lua/plugins/dap-virtual-text.lua
+++ lua/plugins/dap-virtual-text.lua
@@ -4,7 +4,7 @@ return {
     event = 'VeryLazy',
     config = function()
       require('nvim-dap-virtual-text').setup {
-        enabled = true, -- enable this plugin (the default)
+        enabled = false, -- enable this plugin (the default)
         enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
         highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
         highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
