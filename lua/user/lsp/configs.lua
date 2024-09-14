f4bcd495e040e598d19aac132d982a8f76008515
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2024-09-13 20:51:37 -0400

    No need for ts_ls name hacks - Lazy update

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -16,7 +16,7 @@ local servers = {
   'lua_ls',
   'nil_ls',
   'tailwindcss',
-  'tsserver',
+  'ts_ls',
 }
 
 mason_lspconfig.setup({
@@ -44,9 +44,5 @@ for _, server in pairs(servers) do
   if has_custom_opts then
     opts = vim.tbl_deep_extend('force', server_custom_opts, opts)
   end
-  if server == 'tsserver' then
-    lspconfig['ts_ls'].setup(opts)
-  else
-    lspconfig[server].setup(opts)
-  end
+  lspconfig[server].setup(opts)
 end
