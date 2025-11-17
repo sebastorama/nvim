40dc6ddc2b9b4339ad7225753d145f16cdc419af
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2025-11-17 17:42:19 -0400

    Removes supermaven, lazy update, disable obsidian for now

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,19 +7,6 @@ return {
 
   -- DX
   -- { 'github/copilot.vim',         event = 'InsertEnter' },
-  {
-    'supermaven-inc/supermaven-nvim',
-    event = 'InsertEnter',
-    config = function()
-      require('supermaven-nvim').setup({
-        keymaps = {
-          accept_suggestion = '<M-i>',
-          accept_word = '<M-o>',
-          clear_suggestion = '<C-]>',
-        },
-      })
-    end,
-  },
   { 'kazhala/close-buffers.nvim', event = 'BufRead' },
   {
     'echasnovski/mini.surround',
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,6 @@
 return {
   'obsidian-nvim/obsidian.nvim',
+  enabled = false,
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = false,
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
