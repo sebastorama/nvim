42f424a1d4de503ffd0cd3794cdad5226a48fd1c
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2026-04-07 10:00:31 -0400

    disable render markdown / lazy updates

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     -- Make sure to set this up properly if you have lazy=true
     'MeanderingProgrammer/render-markdown.nvim',
-    enabled = true,
+    enabled = false,
     opts = {
       file_types = { 'markdown', 'Avante' },
     },
