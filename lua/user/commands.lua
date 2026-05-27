091b42ee685995375aded00756ff1a8aaa287b44
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2026-05-27 11:40:43 -0300

    commands to delete all other buffers

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -27,6 +27,28 @@ vim.api.nvim_create_user_command('Gr', function(args)
   vim.cmd('GrepperRg ' .. args.args)
 end, { nargs = '*' })
 
+vim.api.nvim_create_user_command('Bov', function()
+  local visible = {}
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    visible[vim.api.nvim_win_get_buf(win)] = true
+  end
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if not visible[buf] and vim.api.nvim_buf_is_loaded(buf) then
+      vim.api.nvim_buf_delete(buf, {})
+    end
+  end
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command('Bo', function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+      vim.api.nvim_buf_delete(buf, {})
+    end
+  end
+end, { nargs = 0 })
+
 -- LSP info command (using native Neovim 0.11+ API)
 vim.api.nvim_create_user_command('LspInfo', function()
   local clients = vim.lsp.get_clients()
